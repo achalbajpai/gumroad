9384eb2e72570c0c4390e7c75e5a6990f0e5f7d2
Achal <achal5bajpai@gmail.com>
2025-07-30 08:53:13 +0530

    update :  Add payment numbering for installment plans

--- app/presenters/receipt_presenter/payment_info.rb
+++ app/presenters/receipt_presenter/payment_info.rb
@@ -153,8 +153,16 @@ class ReceiptPresenter::PaymentInfo
     def today_payment_heading_attribute
       return unless any_upcoming_payments?
 
+      label = if installment_plan?
+        current_installment = subscription_for_installment.purchases.successful.count
+        total_installments = subscription_for_installment.charge_occurrence_count
+        "Today's payment: #{current_installment} of #{total_installments}"
+      else
+        "Today's payment"
+      end
+
       {
-        label: "Today's payment",
+        label: label,
         value: nil
       }
     end
@@ -217,8 +225,17 @@ class ReceiptPresenter::PaymentInfo
     end
 
     def upcoming_payment_heading_attribute
+      label = if installment_plan?
+        current_installment = subscription_for_installment.purchases.successful.count
+        total_installments = subscription_for_installment.charge_occurrence_count
+        next_installment = current_installment + 1
+        "Upcoming payment: #{next_installment} of #{total_installments}"
+      else
+        "Upcoming #{"payment".pluralize(upcoming_price_attributes.size)}"
+      end
+
       {
-        label: "Upcoming #{"payment".pluralize(upcoming_price_attributes.size)}",
+        label: label,
         value: nil
       }
     end
@@ -311,5 +328,13 @@ class ReceiptPresenter::PaymentInfo
 
       tax_percent = tax_rate / taxjar_info.combined_tax_rate.to_f
       amount_cents * tax_percent
+    end
+
+    def installment_plan?
+      subscription_for_installment&.has_fixed_length?
+    end
+
+    def subscription_for_installment
+      @_subscription ||= chargeable.successful_purchases.first&.subscription
     end
 end
